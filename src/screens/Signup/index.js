83b6d7f16a4b85353f2a0394515dfcfa915a478c
Avishek <avishekbhattacharjee35@gmail.com>
2021-06-01 00:35:23 +0530

    Signup form validation and UI

--- src/screens/Signup/index.js
+++ src/screens/Signup/index.js
@@ -1,10 +1,147 @@
-import React from 'react'
-import {View,Text} from 'react-native'
+import React,{useState} from 'react'
+import {View,Text,TextInput,Image, TouchableOpacity} from 'react-native'
+import Container from '../../components/common/Container'
+import styles from './styles'
+import CustomButton from '../../components/common/CustomButton'
+import Input from '../../components/common/Input'
+import { useNavigation } from '@react-navigation/native';
+import { LOGIN } from '../../constants/routeNames'
+import { onChange } from 'react-native-reanimated'
+
+
 const Signup=()=>{
+    const[value,setValue]=useState('')
+    const[form,setForm]=useState({})
+    const[errors,setErrors]=useState({})
+    const onChange=({name,value})=>{
+        setForm({...form,[name]:value})
+        if(value!==''){
+            if(name==='password'){
+                if(value.length < 6){
+                setErrors(prev=>{
+                    return {...prev,[name]:"Password should be greater than 6"}
+                })
+            }
+            }
+            setErrors(prev=>{
+                return {...prev,[name]:null}
+            })
+        }
+        else{
+            setErrors(prev=>{
+                return {...prev,[name]:'This field is required'}
+            })
+        }
+        
+        }
+        
+    
+    const onSubmit=()=>{
+        if(!form.userName){
+            setErrors(prev=>{
+                return {...prev,userName:"Please add an Username"}
+            })
+        }
+        if(!form.firstName){
+            setErrors(prev=>{
+                return {...prev,firstName:"Please add a Firstname"}
+            })
+        }
+        if(!form.lastName){
+            setErrors(prev=>{
+                return {...prev,lastName:"Please add a Lastname"}
+            })
+        }
+        if(!form.password){
+            setErrors(prev=>{
+                return {...prev,password:"Please add a Password"}
+            })
+        }
+        if(!form.email){
+            setErrors(prev=>{
+                return {...prev,email:"Please add an Email"}
+            })
+        }
+
+    }
+    const {navigate}=useNavigation()
     return (
-        <View>
-            <Text>Hi from Signup</Text>
-        </View>
+        <Container>
+        <Image height={70} width={70} source={require('../../assets/images/Signup.png')} style={styles.logo}></Image>
+          <View>
+          <Text style={styles.title}>Welcome to Contax</Text>
+          <Text style={styles.subtitle}>Create a free account</Text>
+          <View style={styles.form}>
+          <Input
+        placeholder="Enter Username"
+        label='Username'    
+        onChangeText={(value)=>{
+            onChange({name:"userName",value})
+        }}
+        iconPosition="right"
+        error={errors.userName}
+      
+      />
+
+        <Input
+        placeholder="Enter Firstname"
+        label='Firstname'    
+        onChangeText={(value)=>{
+            onChange({name:"firstName",value})
+        }}
+        iconPosition="right"
+        error={errors.firstName}
+      
+
+      />
+
+<Input
+        placeholder="Enter Lastname"
+        label='Lastname'    
+        onChangeText={(value)=>{
+            onChange({name:'lastName',value})
+        }}
+        iconPosition="right"
+        error={errors.lastName}
+      
+
+      />
+      <Input
+        placeholder="Enter Email"
+        label='Email'    
+        onChangeText={(value)=>{
+            onChange({name:"email",value})
+        }}
+        iconPosition="right"
+        error={errors.email}
+      
+
+      />
+      
+      <Input
+        icon={<Text>SHOW</Text>}
+        placeholder="Enter password"
+        label='Password'    
+        onChangeText={(value)=>{
+            onChange({name:'password',value})
+        }}
+        iconPosition="right"
+        error={errors.password}
+        secureTextEntry={true}
+       
+      />
+      
+    <CustomButton secondary title="Submit" 
+    onPress={onSubmit}></CustomButton></View>
+     <View style={styles.signupSection}>
+      <Text style={styles.infoText}>Already have an account?</Text>
+      <TouchableOpacity onPress={()=>{navigate(LOGIN)}}><Text style={styles.linkSignup}>Log In</Text></TouchableOpacity>
+    </View>
+    {/* <CustomButton primary title="Submit" loading={true}></CustomButton>
+    <CustomButton secondary title="Submit" loading={true}></CustomButton>
+    <CustomButton danger title="Submit" loading={true}></CustomButton> */}  
+    </View>
+            </Container>
     )
 }
 
---
+++ src/screens/Signup/styles.js
@@ -0,0 +1,47 @@
+import {StyleSheet} from 'react-native';
+import colors from '../../assets/themes/colors';
+
+
+export default StyleSheet.create({
+  logo: {
+    height:170,
+    width:'100%',
+    alignSelf:'center',
+    marginTop:50
+    
+
+  },
+  title:{
+      fontSize:21,
+      textAlign:'center',
+      paddingTop:20,
+      fontWeight:"500"
+      
+  },
+  subtitle:{
+    fontSize:17,
+    textAlign:'center',
+    paddingTop:20,
+    fontWeight:"500"
+    
+},
+form:{
+    paddingTop:20
+},
+signupSection:{
+    flexDirection:'row',
+    alignItems:'center',
+    paddingTop:20
+},
+linkSignup:{
+    paddingLeft:10,
+    color:colors.primary,
+    fontSize:16
+
+},
+infoText:{
+    fontSize:17,
+    paddingLeft:'20%',
+}
+
+});
