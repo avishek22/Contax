6c48e095b10e7395ecae7ad599f227cac68e2538
Avishek <avishekbhattacharjee35@gmail.com>
2021-06-05 10:51:35 +0530

    Login integration

--- src/context/reducers/auth.js
+++ src/context/reducers/auth.js
@@ -1,30 +1,68 @@
-import { REGISTER_FAIL, REGISTER_LOADING, REGISTER_SUCCESS } from "../../constants/actionTypes";
+import {
+  CLEAR_AUTH_STATE,
+  LOGIN_FAIL,
+  LOGIN_LOADING,
+  LOGIN_SUCCESS,
+  LOGOUT_USER,
+  REGISTER_FAIL,
+  REGISTER_LOADING,
+  REGISTER_SUCCESS,
+} from '../../constants/actionTypes';
 
-const auth=(state,{type,payload})=>{
-    switch(type){
-        case REGISTER_LOADING:
-            console.log('state',state)
-            return {
-                ...state,
-                loading:true
-            }
-            case REGISTER_SUCCESS:
-            console.log('state',state)
-            return {
-                ...state,
-                loading:false,
-                data:payload
-            }
-            case REGISTER_FAIL:
-            console.log('state',state)
-            return {
-                ...state,
-                loading:false,
-                error:payload
-            }
-        default:
-            return state;
+const auth = (state, {type, payload}) => {
+  switch (type) {
+    case LOGIN_LOADING:
+    case REGISTER_LOADING:
+        console.log(state)
+      return {
+        ...state,
+        loading: true,
+      };
 
-    }
-}
- export default auth;
+    case REGISTER_SUCCESS:
+        console.log('success',state)
+      return {
+        ...state,
+        loading: false,
+        data: payload,
+      };
+
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: payload,
+        isLoggedIn: true,
+      };
+
+    case LOGOUT_USER:
+      return {
+        ...state,
+        loading: false,
+        data: null,
+        isLoggedIn: false,
+      };
+
+    case REGISTER_FAIL:
+        console.log('fail',state)
+    case LOGIN_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: payload,
+      };
+
+    case CLEAR_AUTH_STATE:
+      return {
+        ...state,
+        loading: false,
+        data: null,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default auth;
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useContext} from 'react'
 import {View,Text,TextInput,Image, TouchableOpacity} from 'react-native'
 import Container from '../../components/common/Container'
 import styles from './styles'
@@ -7,28 +7,47 @@ import Input from '../../components/common/Input'
 import { SIGNUP } from '../../constants/routeNames'
 import { useNavigation } from '@react-navigation/native';
 import Message from '../../components/common/message'
+import { GlobalContext } from '../../context/Provider'
+import loginUser from '../../context/actions/auth/loginUser'
 
 
 const Login=()=>{
-    const[value,setValue]=useState('')
+    const [form,setForm]=useState({})
     const {navigate}=useNavigation()
+    
+    const{authDispatch,authState:{error,loading}}=useContext(GlobalContext)
+    const onSubmit=()=>{
+      if(form.userName && form.password){
+        console.log(form)
+        loginUser(form)(authDispatch)
+      }
+    }
+    const onChange=({name,value})=>{
+      setForm({...form,[name]:value})
+    }
     return (
         <Container>
         <Image height={70} width={70} source={require('../../assets/images/Logo.png')} style={styles.logo}></Image>
           <View>
           <Text style={styles.title}>Welcome to Contax</Text>
           <Text style={styles.subtitle}>Please Login</Text>
-          <Message retry retryFn={()=>{
-            console.log("click")
-          }} message="invalid creds" primary onDismiss={()=>{}}></Message>
+          
           <View style={styles.form}>
+          {error && !error.error && <Message retry retryFn={()=>{
+            console.log("click")
+          }} message="invalid creds" danger onDismiss={()=>{}}></Message>}
+          {error?.error && <Message retry retryFn={()=>{
+            console.log("click")
+          }} message={error?.error} danger onDismiss={()=>{}}></Message>}
           <Input
         placeholder="Enter Username"
         label='Username'    
-        onChangeText={(e)=>{setValue(e)}}
-        value={value}
         iconPosition="right"
-        error={"This field is required"}
+        error={error?.username?.[0]}
+        onChangeText={(value)=>{
+          onChange({name:'userName',value})
+        }}
+        //error={"This field is required"}
       
       />
       
@@ -36,15 +55,18 @@ const Login=()=>{
         icon={<Text>SHOW</Text>}
         placeholder="Enter password"
         label='Password'    
-        onChangeText={(e)=>{setValue(e)}}
-        value={value}
+       
         iconPosition="right"
-        error={"This field is required"}
+        //error={"This field is required"}
+        error={error?.password?.[0]}
+        onChangeText={(value)=>{
+          onChange({name:'password',value})
+        }}
         secureTextEntry={true}
        
       />
       
-    <CustomButton secondary title="Submit" loading={true}disabled={true}></CustomButton></View>
+    <CustomButton secondary title="Submit" loading={loading} disabled={loading} onPress={onSubmit}></CustomButton></View>
     <View style={styles.signupSection}>
       <Text style={styles.infoText}>Need a new account?</Text>
       <TouchableOpacity onPress={()=>{navigate(SIGNUP)}}><Text style={styles.linkSignup}>Sign Up</Text></TouchableOpacity>
