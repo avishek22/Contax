e7e2076787b01c381afb3b4e4be13ee9dc79a9fa
Avishek <avishekbhattacharjee35@gmail.com>
2021-06-08 22:22:48 +0530

    Icon custom

--- src/components/ContactsComponent/index.js
+++ src/components/ContactsComponent/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { View, Text,FlatList, ActivityIndicator } from 'react-native'
+import { View, Text,FlatList, ActivityIndicator,Image } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import colors from '../../assets/themes/colors'
 import AppModal from '../common/AppModal'
 import CustomButton from '../common/CustomButton'
 import Message from '../common/message'
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
+import Icon from '../common/Icon/index'
 import styles from '../ContactsComponent/styles'
 
 const ListEmptyComponent=()=>{
@@ -16,18 +17,18 @@ const ListEmptyComponent=()=>{
 
 const renderItem=({item})=>{
     console.log('item',item)
-    const{contact_picture,first_name,last_name,phone_number}=item
+    const{contact_picture,first_name,last_name,phone_number,id}=item
     return <TouchableOpacity style={styles.itemContainer}>
         <View style={styles.item}>
             {contact_picture?<Image style={{width:45,height:45,borderRadius:100}} source={{uri:contact_picture}}></Image>:
             <View style={{width:45,height:45,backgroundColor:colors.grey}}></View>}
             <View style={{flexDirection:'row'}}>
-                <Text>{first_name}</Text>
+                <TouchableOpacity onPress={()=>{console.log('id',id)}}><Text>{first_name}</Text></TouchableOpacity>
                 <Text>{last_name}</Text>
             </View>
             <Text>{phone_number}</Text>
         </View>
-        <MaterialIcon name="right"></MaterialIcon>
+        <Icon name="right" type="ant"></Icon>
     </TouchableOpacity>
 }
 
