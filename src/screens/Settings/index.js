86d7672b8002c6955968ea0762c593c225f28de8
Avishek <avishekbhattacharjee35@gmail.com>
2021-06-12 17:59:05 +0530

    Settings screen UI and options

--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import {View,Text} from 'react-native'
+import SettingsComponent from '../../components/SettingsComponent'
 const Settings=()=>{
+    const settingsOptions=[
+        {title:"My Info",subTitle:"Setup your Profile",onPress:()=>{
+
+        }},
+        {title:"Accounts",subTitle:null,onPress:()=>{
+
+        }},{title:"Default account for new contacts",subTitle:"Hello@hello.om",onPress:()=>{
+
+        }},{title:"Contacts to display",subTitle:"All Contacts",onPress:()=>{
+
+        }},{title:"Sort by",subTitle:"First name",onPress:()=>{
+
+        }},{title:"Name format",subTitle:"First name first",onPress:()=>{
+
+        }},{title:"Import",subTitle:null,onPress:()=>{
+
+        }},{title:"Export",subTitle:null,onPress:()=>{
+
+        }},{title:"Blocked numbers",subTitle:null,onPress:()=>{
+
+        }},{title:"About Contax",subTitle:null,onPress:()=>{
+
+        }},
+    ]
     return (
-        <View>
-            <Text>Hi from Settings</Text>
-        </View>
+        <SettingsComponent settingsOptions={settingsOptions}></SettingsComponent>
+        
     )
 }
 
