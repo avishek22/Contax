87705ff4348fcd540f89e550577ebae78de83222
Avishek <avishekbhattacharjee35@gmail.com>
2021-06-16 23:25:00 +0530

    dynamic header

--- src/screens/ContactDetail/index.js
+++ src/screens/ContactDetail/index.js
@@ -1,10 +1,18 @@
-import { useRoute } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import React from 'react'
 import {View,Text} from 'react-native'
+import { useEffect } from 'react/cjs/react.development'
 import ContactDetailsComponent from '../../components/ContactDetailsComponent'
 const ContactDetail=()=>{
     const {params:{item={}}={}}=useRoute()
     console.log(`item`, item)
+    const {setOptions}=useNavigation()
+
+    useEffect(()=>{
+        setOptions({
+            title:item.first_name+" "+item.last_name
+        })
+    },[item])
     return (
        <ContactDetailsComponent contact={item}></ContactDetailsComponent>
     )
