0727888eb5ccd66819141d9af622965c37ccf60c
Avishek <avishekbhattacharjee35@gmail.com>
2021-06-16 23:32:46 +0530

    right header icons

--- src/screens/ContactDetail/index.js
+++ src/screens/ContactDetail/index.js
@@ -1,8 +1,10 @@
 import { useNavigation, useRoute } from '@react-navigation/native'
 import React from 'react'
-import {View,Text} from 'react-native'
+import {View,Text, TouchableOpacity} from 'react-native'
 import { useEffect } from 'react/cjs/react.development'
 import ContactDetailsComponent from '../../components/ContactDetailsComponent'
+import Icon from '../../components/common/Icon'
+import colors from '../../assets/themes/colors'
 const ContactDetail=()=>{
     const {params:{item={}}={}}=useRoute()
     console.log(`item`, item)
@@ -10,7 +12,20 @@ const ContactDetail=()=>{
 
     useEffect(()=>{
         setOptions({
-            title:item.first_name+" "+item.last_name
+            title:item.first_name+" "+item.last_name,
+            headerRight:()=>{
+                return<View style={{flexDirection:'row',paddingRight:10}}>
+                    <TouchableOpacity>
+                    <Icon color={colors.grey} size={21} name={item.is_favorite?"star":"star-border"} type="material"></Icon>
+                    
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{paddingLeft:10}}>
+                    <Icon size={21} color={colors.grey} name="delete" type="material"></Icon>
+                    
+                    </TouchableOpacity>
+                    
+                </View>
+            }
         })
     },[item])
     return (
